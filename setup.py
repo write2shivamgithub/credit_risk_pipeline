0c78823a89d768dfe30304eb530d0843cd1fd507
write2shivamgithub <shivamshankaraacharya@gmail.com>
2024-07-30 12:23:53 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small pipeline project related to credit risk and the dataset we are working on a bandhan bank',
+    author='shivam',
+    license='',
+)
